87eda8ff79bfaf8d4cfee01d78649383581f0607
Oreki1001 <trankimhieu1001@gmail.com>
2025-05-29 20:00:50 +0700

    fix bug send mail deploy

--- src/main/java/dtn/ServiceScore/configurations/EmailConfig.java
+++ src/main/java/dtn/ServiceScore/configurations/EmailConfig.java
@@ -1,5 +1,6 @@
 package dtn.ServiceScore.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,14 +10,24 @@ import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
 
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
     @Bean
     public JavaMailSender mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");  // Địa chỉ máy chủ SMTP
-        mailSender.setPort(587);  // Cổng SMTP (thường là 587 cho TLS, 465 cho SSL)
-        mailSender.setUsername("trankimhieu1008@gmail.com");  // Tên đăng nhập email
-        mailSender.setPassword("lldq uzsm agwe vqwi");  // Mật khẩu email
+        mailSender.setHost(host);  // Địa chỉ máy chủ SMTP
+        mailSender.setPort(port);  // Cổng SMTP (thường là 587 cho TLS, 465 cho SSL)
+        mailSender.setUsername(username);  // Tên đăng nhập email
+        mailSender.setPassword(password);  // Mật khẩu email
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
